7ae2bd85b28d0c691b86bc4b6980517ab0d0cb59
RickyITPH <se77mail@gmail.com>
2019-02-14 10:11:17 +0100

    added Backend Rate structure, tables of rate is now working in db.

--- src/main/java/com/Beeragood/Beeragood/model/Rate.java
+++ src/main/java/com/Beeragood/Beeragood/model/Rate.java
@@ -1,4 +1,87 @@
 package com.Beeragood.Beeragood.model;
 
+import javax.persistence.*;
+
+@Entity
 public class Rate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private int cijfer;
+    private String omschrijving;
+    private String datum;
+    private String locatie;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Bier bier;
+
+    public Rate() {}
+
+    public Rate(int id, int cijfer, String omschrijving, String datum, String locatie, User user, Bier bier) {
+        this.id = id;
+        this.cijfer = cijfer;
+        this.omschrijving = omschrijving;
+        this.datum = datum;
+        this.locatie = locatie;
+        this.user = user;
+        this.bier = bier;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getCijfer() {
+        return cijfer;
+    }
+
+    public void setCijfer(int cijfer) {
+        this.cijfer = cijfer;
+    }
+
+    public String getOmschrijving() {
+        return omschrijving;
+    }
+
+    public void setOmschrijving(String omschrijving) {
+        this.omschrijving = omschrijving;
+    }
+
+    public String getDatum() {
+        return datum;
+    }
+
+    public void setDatum(String datum) {
+        this.datum = datum;
+    }
+
+    public String getLocatie() {
+        return locatie;
+    }
+
+    public void setLocatie(String locatie) {
+        this.locatie = locatie;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Bier getBier() {
+        return bier;
+    }
+
+    public void setBier(Bier bier) {
+        this.bier = bier;
+    }
 }
+
--- src/main/java/com/Beeragood/Beeragood/services/RateService.java
+++ src/main/java/com/Beeragood/Beeragood/services/RateService.java
@@ -1,9 +1,9 @@
 package com.Beeragood.Beeragood.services;
 
-import com.Beeragood.Beeragood.model.Bier;
+import com.Beeragood.Beeragood.model.Rate;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BierService extends CrudRepository<Bier, Integer > {
+public interface RateService extends CrudRepository<Rate, Integer > {
 }
