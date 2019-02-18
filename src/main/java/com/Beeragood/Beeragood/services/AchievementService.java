4c3a87a116df901d9cfb71999e66bbd4a06e3f7b
RickyITPH <se77mail@gmail.com>
2019-02-18 15:28:37 +0100

    added show of score in achievement page

--- src/main/java/com/Beeragood/Beeragood/services/AchievementService.java
+++ src/main/java/com/Beeragood/Beeragood/services/AchievementService.java
@@ -1,6 +1,7 @@
 package com.Beeragood.Beeragood.services;
 
 import com.Beeragood.Beeragood.model.Achievement;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
